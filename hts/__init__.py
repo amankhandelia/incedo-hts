3338e01131f2009ecb5d89beec630a15322ac592
Deepak Raj Subramanian <deepak.rs@incedoinc.com>
2020-08-10 21:16:49 -0400

    rollling back the change

--- hts/__init__.py
+++ hts/__init__.py
@@ -4,7 +4,7 @@ from hts.revision import RevisionMethod
 
 __author__ = """Carlo Mazzaferro"""
 __email__ = 'carlo.mazzaferro@gmail.com'
-__version__ = '0.5.3'
+__version__ = '0.5.2'
 
 
 __all__ = [
